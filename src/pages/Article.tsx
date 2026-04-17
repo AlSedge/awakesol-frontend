0f24803a99407e4ed793b01428ba098fa5b8c037
Your NameAlan <ags@awakesol.com>
2026-04-17 09:29:38 +0100

    Removed React imports

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { ArrowLeft, Calendar, Loader2 } from 'lucide-react';
 import Navigation from '../components/Navigation';
